2b9f71e5bec3b4ece614ee5ff160e3a861541443
elsayedfarouk <elsayedfarouk874@gmail.com>
2025-09-02 14:21:30 +0300

    update

--- sportsnews.py
+++ sportsnews.py
@@ -5,6 +5,7 @@ def main2():
     """Main execution function"""
     processor = main.NewsProcessor(country='US')
 
+
     # Uncomment to process topic news
     processor.process_topic_news(["SPORTS"], spreadsheet_name='NewsToday', sheet_name='Sports')
 
