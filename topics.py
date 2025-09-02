d97eabdac992dfd9ba8c32af13a654581de5e5c6
elsayedfarouk <elsayedfarouk874@gmail.com>
2025-09-02 17:44:05 +0300

    update

--- topics.py
+++ topics.py
@@ -6,7 +6,7 @@ def main2():
     processor = main.NewsProcessor(country='US')
 
     # Uncomment to process topic news
-    processor.process_topic_news(["BUSINESS", "TECHNOLOGY", "ENTERTAINMENT", "SPORTS", "SCIENCE", "HEALTH"])
+    processor.process_topic_news(["BUSINESS", "TECHNOLOGY", "SCIENCE", "HEALTH"])
 
 
 if __name__ == "__main__":
