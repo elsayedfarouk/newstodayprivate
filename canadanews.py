765773506d6fa6e28b60be63056d2f9f1a0e9d3e
elsayedfarouk <elsayedfarouk874@gmail.com>
2025-09-12 23:49:27 +0300

    update

--- canadanews.py
+++ canadanews.py
@@ -12,7 +12,7 @@ def main2():
     processor.process_latest_news(spreadsheet_name='CanadaNewsToday', sheet_name='News', voice=voice)
 
     # Uncomment to process topic news
-    processor.process_topic_news(topics=topics, spreadsheet_name='CanadaNewsToday', sheet_name='News', voice=voice)
+    # processor.process_topic_news(topics=topics, spreadsheet_name='CanadaNewsToday', sheet_name='News', voice=voice)
 
 
 if __name__ == "__main__":
