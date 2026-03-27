4afcc7febf2cf226d8ac4d8ea0c361076e824459
elsayedfarouk <elsayedfarouk874@gmail.com>
2026-03-27 04:18:50 +0300

    update

--- post_to_facebook.py
+++ post_to_facebook.py
@@ -57,13 +57,15 @@ def post_video_from_url(video_url: str,
 
 def main():
     # --- Config ---
-    VIDEO_URL = "https://github.com/elsayedfarouk/public/raw/refs/heads/main/news_videos/20260207/0LUF1Aqk9M.mp4"
+    VIDEO_URL = "https://github.com/elsayedfarouk/public/raw/refs/heads/main/news_videos/20260327/x9RdY6bSVT.mp4"
 
     # --- Post Video ---
     result = post_video_from_url(
         video_url=VIDEO_URL,
-        title="My Video Title",
-        description="Uploaded via Python 🎬"
+        title="",
+        description="""
+        Trump says he’ll sign order to pay TSA agents as Congress struggles to reach funding deal', 'date': 'Thu, 26 Mar 2026 22:43:00 GMT', 'summary': "President Trump announced he will sign an order to ensure Transportation Security Administration agents receive pay during the ongoing partial government shutdown. This action comes as Congress continues to struggle to reach a funding agreement, now in its 41st day, causing significant travel delays and financial hardship for federal workers. The White House had considered declaring a national emergency to address the issue, but the president may instead redirect existing funds. Democrats are seeking concessions on immigration enforcement policies, specifically regarding ICE operations, including agent identification and restrictions on raids. Republicans have presented a final offer, but negotiations remain stalled. Airports are experiencing increased TSA worker absences, with over 3,100 callouts Wednesday and nearly 500 officers resigning during the shut
+        """
     )
 
     print(result)
